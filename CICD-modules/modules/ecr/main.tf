f237071923283ab350e64f6df2995f8c623dbc7a
BenNguyen97 <thanhluan28297@gmail.com>
2025-03-14 20:41:14 +0700

    test lan 7

--- CICD-modules/modules/ecr/main.tf
+++ CICD-modules/modules/ecr/main.tf
@@ -8,5 +8,5 @@ resource "aws_ecr_repository" "aws_ecr" {
     image_scanning_configuration {
         scan_on_push = true
     }
-    tags                    = { Name = "luan's ECR" }
+    tags                    = { Name = "luan's ecr" }
 }
